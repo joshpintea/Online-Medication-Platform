ed153923d2b10356f462776b2072f29915287a55
Josh Pintea <pintea.josh@gmail.com>
2020-01-06 19:41:55 +0200

    initial data

--- java_soap_web_service/src/main/resources/data.sql
+++ java_soap_web_service/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 insert into user values (1, 'josh', now(), 'M', 'josh', 'josh', 'ROLE_DOCTOR', 'josh');
-insert into user values (2, 'marin', now(), 'M', 'marin', 'marin', 'ROLE_DOCTOR', 'marin');
+insert into user values (2, 'patient', now(), 'M', 'patient', 'patient', 'ROLE_PATIENT', 'patient');
+insert into user values (3, 'caregiver', now(), 'M', 'caregiver', 'caregiver', 'ROLE_CAREGIVER', 'caregiver');
 
-insert into caregiver values (1);
-
-insert into patient values (2, 1);
+insert into doctor values (1);
+insert into caregiver values (3);
+insert into patient values (2, 3);
